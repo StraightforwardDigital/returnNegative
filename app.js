601753f0a92218e990bd807cca522820c498ca90
StraightforwardDigital <111469465+StraightforwardDigital@users.noreply.github.com>
2023-06-10 11:27:29 -0300

    adding js file

---
+++ app.js
@@ -0,0 +1,9 @@
+function makeNegative(num) {
+  // conditional that returns number as is if greter than or equal to 0
+  if (num <= 0) {
+    return(num)
+  // applies - and returns number if greater than 0
+  } else {
+    return(-num)
+  }
+}
